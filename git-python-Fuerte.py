20736157e503fa4ee2834b03d63aec80573183a3
FuerteSeannWilliam <seannfuerte7@gmail.com>
2024-02-15 17:48:22 +0800

    My Personal Info added

---
+++ git-python-Fuerte.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 15 17:27:24 2024
+
+@author: Seann William M. Fuerte
+"""
+
+print ("Full Name: Seann William M. Fuerte")
+print ("Address: Bunganay, Boac, Marinduque")
+print ("Birthday: 10-07-2003")
+
